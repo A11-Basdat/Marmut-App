4a83468081d08402bd676604d8abb6f284bfffb2
SandriaRania <sandria.rania@ui.ac.id>
2024-04-29 22:41:22 +0700

    Add Marmut dashboard dan tambah register label (pengguna belum)

--- marmut/urls.py
+++ marmut/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('authentication/', include('authentication.urls'))
+    path('authentication/', include('authentication.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ]
