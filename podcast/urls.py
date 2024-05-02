5c461107b6c54b5af00a40a2acc5ded2024083aa
nadriha <hafez.nadhira@gmail.com>
2024-05-02 10:35:06 +0700

    add create list podcast page

--- podcast/urls.py
+++ podcast/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from podcast.views import create_podcast
+from podcast.views import create_podcast, list_podcast
 
 app_name = 'podcast'
 
 urlpatterns = [
     path('create/', create_podcast, name='create_podcast'),
+    path('all-podcast/', list_podcast, name='list_podcast'),
 ]
+
--- podcast/views.py
+++ podcast/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def create_podcast(request):
     return render(request, "createPodcast.html")
+
+def list_podcast(request):
+    return render(request, "listPodcast.html")
