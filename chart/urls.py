2a936a5c3a03bc2c3775d75c10ff5d37ea28b3c6
nadriha <hafez.nadhira@gmail.com>
2024-05-02 13:30:24 +0700

    add detail chart page

--- chart/urls.py
+++ chart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from chart.views import list_chart
+from chart.views import list_chart, detail_chart
 
 app_name = 'chart'
 
 urlpatterns = [
     path('', list_chart, name='list_chart'),
+    path('detail/', detail_chart, name='detail_chart'),
 ]
--- chart/views.py
+++ chart/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def list_chart(request):
     return render(request, "listChart.html")
+
+def detail_chart(request):
+    return render(request, "detailChart.html")
