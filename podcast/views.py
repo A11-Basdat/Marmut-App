380a3e1d47ac1d694939d56f698e05a99411cb9f
nadriha <hafez.nadhira@gmail.com>
2024-05-17 12:09:47 +0700

    Create Podcast

--- podcast/views.py
+++ podcast/views.py
@@ -1,10 +1,69 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.db import connection
+from django.db import connection, DatabaseError
 from .query import get_podcast_query
 from .format import format_podcast_data
+import uuid
+from datetime import datetime
 
 def create_podcast(request):
+    cursor = connection.cursor()
+
+    if request.method == 'POST':
+        judul = request.POST.get('judul')
+        genre = request.POST.get('genre')
+        durasi = request.POST.get('durasi')
+
+        new_uuid = str(uuid.uuid4())
+        current_datetime = datetime.now()
+        formatted_date = current_datetime.strftime('%Y-%m-%d')
+        current_year = current_datetime.year
+
+        try: 
+            cursor.execute(f"""
+                INSERT INTO KONTEN 
+                VALUES ('{new_uuid}', '{judul}', '{formatted_date}', '{current_year}', '{durasi}');
+            """)
+
+            cursor.execute(f"""
+                INSERT INTO GENRE VALUES 
+                ('{new_uuid}', '{genre}');
+            """)
+
+            cursor.execute(f"""
+                INSERT INTO PODCAST VALUES 
+                ('{new_uuid}', 'laisha07@example.net');
+            """)
+
+            cursor.execute(f"""
+                SELECT KONTEN.judul, GENRE.genre, KONTEN.durasi 
+                FROM KONTEN, GENRE 
+                WHERE KONTEN.id='{new_uuid}' AND GENRE.id_konten=KONTEN.id;
+            """)
+            create_result = cursor.fetchone()
+
+            
+            context = {
+                'judul': create_result[0],
+                'genre': create_result[1],
+                'durasi': create_result[2],
+                'error': None
+            }
+
+        except DatabaseError as e:
+            # Handle database errors
+            context = {
+                'judul': None,
+                'genre': None,
+                'durasi': None,
+                'error': str(e)
+            }
+
+        return render(request, "createPodcast.html", context)
+        
+        
+
+
     return render(request, "createPodcast.html")
 
 def list_podcast(request):
