dc6dadfd9599591e7ec7180484ffc0cd860a5f33
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2024-05-19 17:40:29 +0700

    feat: downloaded_song only accessible for premium user

--- song/views.py
+++ song/views.py
@@ -11,19 +11,31 @@ def song(request):
 def downloaded_song(request):
     if 'email' not in request.session:
         return redirect('/authentication/login/')
+    
     email = request.session['email']
     cursor = connection.cursor()
     cursor.execute("""
-        SELECT k.judul, a.nama
-        FROM downloaded_song ds
-        JOIN konten k ON ds.id_song = k.id
-        JOIN song s ON k.id = s.id_konten
-        JOIN artist ar ON s.id_artist = ar.id
-        JOIN akun a ON a.email = ar.email_akun
-        WHERE ds.email_downloader = %s;
+        SELECT COUNT(*) FROM premium WHERE email = %s;
     """, [email])
-    results = cursor.fetchall()
-    context = {
-        'results': results
-    }
+    premium_count = cursor.fetchone()[0]
+
+    if premium_count == 0:
+        context = {
+            'is_premium': False
+        }
+    else:
+        cursor.execute("""
+            SELECT k.judul, a.nama
+            FROM downloaded_song ds
+            JOIN konten k ON ds.id_song = k.id
+            JOIN song s ON k.id = s.id_konten
+            JOIN artist ar ON s.id_artist = ar.id
+            JOIN akun a ON a.email = ar.email_akun
+            WHERE ds.email_downloader = %s;
+        """, [email])
+        results = cursor.fetchall()
+        context = {
+            'results': results,
+            'is_premium': True
+        }
     return render(request, "downloaded_song.html", context)
